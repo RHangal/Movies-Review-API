74593071edbf6afe9b0159a7f05338ad7d341de9
Rohan Hangal <rohanhangal@gmail.com>
2023-07-25 16:12:25 -0700

    cors

--- src/main/java/app/MovieAPI/movies/MovieController.java
+++ src/main/java/app/MovieAPI/movies/MovieController.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:5173/")
 @RestController
 @RequestMapping("/api/v1/movies")
+@CrossOrigin(origins = "http://localhost:5173")
 public class MovieController {
     @Autowired
     private MovieService movieService;
