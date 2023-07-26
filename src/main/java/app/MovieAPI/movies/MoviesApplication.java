3a6d5ca69dd5c6c13c825dc551cead25178c3035
Rohan Hangal <rohanhangal@gmail.com>
2023-07-25 17:23:24 -0700

    test

--- src/main/java/app/MovieAPI/movies/MoviesApplication.java
+++ src/main/java/app/MovieAPI/movies/MoviesApplication.java
@@ -3,6 +3,7 @@ package app.MovieAPI.movies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,7 +19,11 @@ public class MoviesApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/*").allowedOrigins("*");
+				registry
+						.addMapping("/**")
+						.allowedMethods(CorsConfiguration.ALL)
+						.allowedHeaders(CorsConfiguration.ALL)
+						.allowedOriginPatterns(CorsConfiguration.ALL);
 			}
 		};
 	}
